1d07b0641dcbd25da3deddf0426863e9dfecf6ae
Justin Peck <jdpeck90@gmail.com>
2019-02-08 11:12:12 -0500

    finish ngFor, conditional styling practice project

--- ngFor-addition/src/app/app.component.ts
+++ ngFor-addition/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'ngFor-addition';
+  title = "ngFor-addition";
+  displaySecret = false;
+  clickArray = [];
+
+  onDisplaySecret() {
+    this.displaySecret = !this.displaySecret;
+    this.clickArray.push(new Date());
+  }
 }
