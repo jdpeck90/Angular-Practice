72fdb8bd6764d05739bae73decd68815219ea596
Justin Peck <jdpeck90@gmail.com>
2019-02-08 16:31:35 -0500

    created and rendered components

---
+++ recipesAndShopping/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { ShoppingComponent } from "./shopping/shopping.component";
+import { RecipeComponent } from "./recipe/recipe.component";
+import { RecipeDetailComponent } from "./recipe/recipe-detail/recipe-detail.component";
+import { RecipeListComponent } from "./recipe/recipe-list/recipe-list.component";
+import { RecipeItemComponent } from "./recipe/recipe-list/recipe-item/recipe-item.component";
+import { ShoppingEditComponent } from "./shopping/shopping-edit/shopping-edit.component";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ShoppingComponent,
+    RecipeComponent,
+    RecipeDetailComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    ShoppingEditComponent
+  ],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
