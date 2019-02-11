d925ace54c23d5d59404020549b3a290f528fef1
Justin Peck <jdpeck90@gmail.com>
2019-02-11 11:35:39 -0500

    get ingredient list working

--- recipesAndShopping/src/app/shopping/shopping.component.ts
+++ recipesAndShopping/src/app/shopping/shopping.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+import { Ingredient } from "../shared/ingredient.module";
 
 @Component({
-  selector: 'app-shopping',
-  templateUrl: './shopping.component.html',
-  styleUrls: ['./shopping.component.scss']
+  selector: "app-shopping",
+  templateUrl: "./shopping.component.html",
+  styleUrls: ["./shopping.component.scss"]
 })
 export class ShoppingComponent implements OnInit {
+  ingredients = [new Ingredient("Apples", 5), new Ingredient("Onions", 5)];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
