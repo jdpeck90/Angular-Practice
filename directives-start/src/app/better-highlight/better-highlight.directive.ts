c6fc74e4b2b79544bfc22d94edd9cfba92ad6c41
Justin Peck <jdpeck90@gmail.com>
2019-02-14 16:53:34 -0500

    add HostBinding and Input to dictate fontColor & backgroundColor

--- directives-start/src/app/better-highlight/better-highlight.directive.ts
+++ directives-start/src/app/better-highlight/better-highlight.directive.ts
@@ -3,16 +3,28 @@ import {
   OnInit,
   ElementRef,
   Renderer2,
-  HostListener
+  HostListener,
+  HostBinding,
+  Input
 } from "@angular/core";
 
 @Directive({
   selector: "[appBetterHighlight]"
 })
 export class BetterHighlightDirective implements OnInit {
+  @Input() defaultColor: string = "yellow";
+  @Input() highlightColor: string = "blue";
+  @Input() defaultFontColor: string "black";
+  @Input() highlightFontColor: string "white";
+  @HostBinding("style.backgroundColor") backgroundColor: string = this
+    .defaultColor;
+  @HostBinding("style.color") fontColor: string = this.defaultFontColor;
+
   constructor(private elRef: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit() {
+    this.backgroundColor = this.defaultColor;
+    this.fontColor = this.defaultFontColor;
     // this.renderer.setStyle(
     //   this.elRef.nativeElement,
     //   "background-color",
@@ -21,18 +33,25 @@ export class BetterHighlightDirective implements OnInit {
   }
 
   @HostListener("mouseenter") mouseover(eventData: Event) {
-    this.renderer.setStyle(
-      this.elRef.nativeElement,
-      "background-color",
-      "blue"
-    );
+    // this.renderer.setStyle(
+    //   this.elRef.nativeElement,
+    //   "background-color",
+    //   "blue",
+    //   false,
+    //   false
+    // );
+    // this.backgroundColor = "blue";
+    this.color = this.highlightFontColor;
+    this.backgroundColor = this.highlightColor;
   }
 
   @HostListener("mouseleave") mouseleave(eventData: Event) {
-    this.renderer.setStyle(
-      this.elRef.nativeElement,
-      "background-color",
-      "transparent"
-    );
+    // this.renderer.setStyle(
+    //   this.elRef.nativeElement,
+    //   "background-color",
+    //   "transparent"
+    // );
+    this.color = this.defaultFontColor;
+    this.backgroundColor = this.defaultColor;
   }
 }
