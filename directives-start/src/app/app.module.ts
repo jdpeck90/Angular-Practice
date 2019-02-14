13112aa9f87ded2e1661471998b33f20231f379e
Justin Peck <jdpeck90@gmail.com>
2019-02-14 12:39:34 -0500

    creating custom directives

--- directives-start/src/app/app.module.ts
+++ directives-start/src/app/app.module.ts
@@ -5,9 +5,14 @@ import { HttpModule } from "@angular/http";
 
 import { AppComponent } from "./app.component";
 import { BasicHighlightDirective } from "./basic-highlight/basic-highlight.directive";
+import { BetterHighlightDirective } from "./better-highlight/better-highlight.directive";
 
 @NgModule({
-  declarations: [AppComponent, BasicHighlightDirective],
+  declarations: [
+    AppComponent,
+    BasicHighlightDirective,
+    BetterHighlightDirective
+  ],
   imports: [BrowserModule, FormsModule, HttpModule],
   providers: [],
   bootstrap: [AppComponent]
